b44c90671941a2ed4ae6d328feae5affb54f1a15
suelazeqo <suelazeqo19@gmail.com>
2025-04-08 11:41:24 +0200

    Added environment.prod.ts file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import {ApplicationConfig, InjectionToken} from '@angular/core';
 import {provideHttpClient, withFetch} from '@angular/common/http';
 import {provideZoneChangeDetection} from '@angular/core';
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
-import {environment} from '../environment';
+import {environment} from '../environments/environment';
 import {provideNativeDateAdapter} from '@angular/material/core';
 
 export const API_URL = new InjectionToken<string>('API_URL', {
